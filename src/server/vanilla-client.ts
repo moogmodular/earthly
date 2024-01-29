725a631ceb8f023cd45db20d7343b90fb7bad1aa
moogmodular <mmodular@protonmail.com>
2024-01-29 21:21:02 +0100

    feat: fix vanilla client

--- src/server/vanilla-client.ts
+++ src/server/vanilla-client.ts
@@ -2,11 +2,17 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client"
 import superjson from "superjson"
 import { type AppRouter } from "./api/root"
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return "" // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
+}
+
 export const vanillaClient = createTRPCClient<AppRouter>({
   transformer: superjson,
   links: [
     httpBatchLink({
-      url: `http://localhost:${process.env.PORT ?? 3000}/api/trpc`,
+      url: `${getBaseUrl()}/api/trpc`,
     }),
   ],
 })
